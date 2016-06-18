a70712214d006586d3ff8ef81c988b2a29f44de5
jdgonzales09 <jd_gonzales@icloud.com>
2016-06-17 22:43:18 -0500

    Complete checkpoint 31 assignment. Change every 5th post to SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |x, i|
+      if i % 5 == 0
+        @posts.title == "SPAM"
+      end
+    end
   end
 
   def show
