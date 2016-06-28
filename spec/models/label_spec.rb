bcd63dcd9121370cba201021a99b3f06890065cf
jdgonzales09 <jd_gonzales@icloud.com>
2016-06-28 02:21:37 -0500

    Add FactoryGirl tests for comments, labels, & votes. Add post count to user profile. Assignment Incomplete.

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe Label, type: :model do
     let(:topic) { create(:topic) }
     let(:user) { create(:user) }
     let(:post) { create(:post) }
-    let(:label) { Label.create!(name: 'Label') }
-    let(:label2) { Label.create!(name: 'Label2') }    
-    
+    let(:label) { create(:label) }
+
     it { is_expected.to have_many :labelings }
     it { is_expected.to have_many(:topics).through(:labelings) }
     it { is_expected.to have_many(:posts).through(:labelings) }
@@ -18,17 +17,18 @@ RSpec.describe Label, type: :model do
 
             topic_label = topic.labels[0]
             post_label = post.labels[0]
-            
+
             expect(topic_label).to eql(post_label)
         end
     end
 
     describe ".update_labels" do
         it "takes a coma delimited string and returns an array of Labels" do
+            label2 = Label.create!(name: 'Label2') 
             labels = "#{label.name}, #{label2.name}"
             labels_as_a = [label, label2]
             expect(Label.update_labels(labels)).to eq(labels_as_a)
         end
     end
-        
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Vote, type: :model do
     let(:topic) { create(:topic) }
     let(:user) { create(:user) }
     let(:post) { create(:post) }
-    let(:vote) { Vote.create!(value: 1, post: post, user: user) }
+    let(:vote) { create(:vote) }
 
     it { is_expected.to belong_to(:post) }
     it { is_expected.to belong_to(:user) }
@@ -21,6 +21,7 @@ RSpec.describe Vote, type: :model do
 
         it "#update_post should call update_rank on post " do
             expect(post).to receive(:update_rank).at_least(:once)
+            post.votes.create!(value: 1)
             vote.save!
         end
     end
