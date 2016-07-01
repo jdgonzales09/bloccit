fd14c4f56df29697d56b2c1e870f0e6d1541a839
jdgonzales09 <jd_gonzales@icloud.com>
2016-06-30 19:01:37 -0500

    Add ajax to votes. Tests pass.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,24 +4,32 @@ class VotesController < ApplicationController
 
     def up_vote
         update_vote(1)
-        redirect_to :back
+
+        respond_to do |format|
+            format.html
+            format.js
+        end
     end
 
     def down_vote
         update_vote(-1)
-        redirect_to :back
+        respond_to do |format|
+            format.html
+            format.js
+        end
     end
 
     private
 
     def update_vote(new_value)
-        @post = Post.find(params[:post_id])
-        @vote = @post.votes.where(user_id: current_user.id).first
+      @post = Post.find(params[:post_id])
+      @vote = @post.votes.where(user_id: current_user.id).first
 
-        if @vote
-            @vote.update_attribute(:value, new_value)
-        else
-            @vote = current_user.votes.create(value: new_value, post: @post)
-        end
+      if @vote
+        @vote.update_attribute(:value, new_value)
+      else
+        @vote = current_user.votes.create(value: new_value, post: @post)
+      end
+
     end
 end
